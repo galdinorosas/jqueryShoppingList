a98d21f50114c036f604b03b177e5b41d533a5d7
galdinorosas <dino.rosas@gmail.com>
2016-02-29 13:37:23 -0800

    working on the deletel button, some type of bubbling effect is at play even though the listed items are not nested within eachother but are all children.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
         e.preventDefault();
     });
 
+
     var itemArray = [];
 
     $("form").submit(function() {
@@ -23,15 +24,52 @@ $(document).ready(function() {
 
         itemArray.push(itemObject);
 
-        var itemHTML = '<div class="itemContainer"> <input type="checkbox" class="itemCheckbox"> <p class="itemText"> Name: <span class="itemName">'+itemObject.name+'</span> Total Cost $: <span class="itemTotalCost">'+itemObject.price+'</span> Item Count: <span class="itemTotalCount">'+itemObject.count+'</span> </p> <input value="DELETE" type="button" class="deleteButton"> </div>';
+        var itemHTML = '<div class="itemContainer"> <input type="checkbox" class="itemCheckbox"> <p class="itemText"> Name: <span class="itemName">'+itemObject.name+'</span> Item Cost $: <span class="itemPrice">'+itemObject.price+'</span> Item Count: <span class="itemTotalCount">'+itemObject.count+'</span> </p> <input value="DELETE" type="button" class="deleteButton"> </div>';
 
         $('.itemList').append(itemHTML);
         console.log(itemArray);
 
-        
+        if(itemArray.length === 5){
+    		$('input[type="submit"]').prop('disabled', true);
+
+	    }
+	    else{
+	    	$('input[type="submit"]').prop('disabled', false);
+	    }
+		
+		$('.itemContainer').on('click','.deleteButton', function(event){
+			// $(this).parent().remove();
+
+			// console.log($(this).parent());
+
+            event = event || window.event // cross-browser event
+            
+            if (event.stopPropagation) {
+                // W3C standard variant
+                event.stopPropagation()
+            } 
+            else {
+                    // IE variant
+                    event.cancelBubble = true
+                }
+            
+
+			var index = $(event.target).parent().index();
+            console.log(index);
+
+            $(event.currentTarget.parentElement).remove();
+
+            itemArray.splice($(event.target.parentElement),1);
+            console.log(itemArray);
+
+            console.log('itemList:',$('.itemList').children().length);
+		});
 
 
     });
+
+	
+
 
 
 
